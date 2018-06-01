1e10593476721c9e94ca49778a55ba15f959520a
yashs2399 <yashs2399@gmail.com>
2018-06-01 12:13:25 +0530

    First Commit

--- server.js
+++ server.js
@@ -1,23 +1,84 @@
-var td = 31;
-var tm = 5;
-var ty = 2018;
-var bd = 1 ;
-var bm = 6;
-var by = 1947;
-var leapYear = ty%4;
-var totalDays;
-var dd=td-bd;
-var dm=tm-bm;
-var dy=ty-by;
- if (dd<0 && ((tm===1) ||(tm===3)||(tm===5)||(tm===7)||(tm===8)||(tm===10) || (tm===12)))
-      {  dd += 31; dm--; }
- else if ((dd<0) && (tm===2) && (leapYear===0))
-         {dd +=29; dm--;}
-  else  if ((dd<0) && (tm===2))
-       {dd +=28; dm--;}
-  else if ((dd<0) && ((tm=== 4) || (tm=== 6) || (tm===9) || (tm=== 11)))
-        {dd+=30; dm--;}
- if (dm<0)
- 	    {dy--; dm+=12;}
- 	totalDays=dy*365.25+dm*30.55+dd;
-alert("Your current age is found to be"+ dy+ " years "+ dm +" months and "+ dd +"  days and the number of days you lived are " + totalDays);
+function calculateAge1(){
+		var t  = new Date(),
+			td = t.getUTCDate(),
+			tm = t.getUTCMonth() + 1,
+			ty = t.getUTCFullYear(),
+			b  = new Date(document.querySelector("#bday").value),
+			bd = b.getUTCDate(),
+			bm = b.getUTCMonth() + 1,
+			by = b.getUTCFullYear(),
+			ly = ty % 4,
+			dd = td - bd,
+			dm = tm - bm,
+			dy = ty - by;
+		if (dd<0 && ((tm===1) || (tm===3) ||(tm===5) || (tm===7) || (tm===8) || (tm===10) || (tm===12))){
+			dd += 31;
+			dm--;
+		} else if ((dd<0) && (tm===2) && (ly===0)){
+			dd += 29;
+			dm--;
+		} else if ((dd<0) && (tm===2)){
+			dd += 28;
+			dm--;
+		} else if ((dd<0) && ((tm=== 4) || (tm=== 6) || (tm===9) || (tm=== 11))){
+			dd += 30;
+			dm--;
+		}
+		if (dm<0){
+			dy--;
+			dm += 12;
+		}
+		if (b<=t){
+		var totalDays = dy * 365.25 + dm * 30.55 + dd;
+		totalDays=Math.floor(totalDays);
+		document.querySelector("#result1").innerHTML = "Your current age is found to be " + dy + " years " + dm + " months and " + dd + "  days and the number of days you lived are " + totalDays + " !";
+	     }else {
+			document.querySelector("#result1").innerHTML = "Future dates are not acceptable!";
+	}}
+	function resetResult1(){
+		document.querySelector("#bday").value = "";
+		document.querySelector("#result1").innerHTML = "";
+	}
+ 
+ function calculateAge2(){
+		var t  = new Date(document.querySelector("#day1").value),
+			td = t.getUTCDate(),
+			tm = t.getUTCMonth() + 1,
+			ty = t.getUTCFullYear(),
+			b  = new Date(document.querySelector("#day2").value),
+			bd = b.getUTCDate(),
+			bm = b.getUTCMonth() + 1,
+			by = b.getUTCFullYear(),
+			ly = ty % 4,
+			dd = td - bd,
+			dm = tm - bm,
+			dy = ty - by;
+		if (dd<0 && ((tm===1) || (tm===3) ||(tm===5) || (tm===7) || (tm===8) || (tm===10) || (tm===12))){
+			dd += 31;
+			dm--;
+		} else if ((dd<0) && (tm===2) && (ly===0)){
+			dd += 29;
+			dm--;
+		} else if ((dd<0) && (tm===2)){
+			dd += 28;
+			dm--;
+		} else if ((dd<0) && ((tm=== 4) || (tm=== 6) || (tm===9) || (tm=== 11))){
+			dd += 30;
+			dm--;
+		}
+		if (dm<0){
+			dy--;
+			dm += 12;
+		}
+		if (b<=t){
+		var totalDays = dy * 365.25 + dm * 30.55 + dd;
+		totalDays=Math.floor(totalDays);
+		document.querySelector("#result2").innerHTML = "The difference between two dates is " + dy + " years " + dm + " months and " + dd + "  days and the number of days are " + totalDays + " !";
+		} else   document.querySelector("#result2").innerHTML = "Please keep the first date greater!"
+}
+	 function resetResult2(){
+		document.querySelector("#day1").value = "";
+		document.querySelector("#day2").value = "";
+		document.querySelector("#result2").innerHTML = "";
+	}
+ 
